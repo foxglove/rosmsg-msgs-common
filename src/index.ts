f8cc29f4399fc1a79b52df9e5965232c05bff217
John Hurliman <jhurliman@jhurliman.org>
2021-07-06 11:21:44 -0700

    Add node typedefs, lint

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { parse, RosMsgDefinition } from "@foxglove/rosmsg";
 import { mkdir, readdir, readFile, writeFile } from "fs/promises";
 import { join, basename, sep } from "path";
 import { format, Options } from "prettier";
-
-import { parse, RosMsgDefinition } from "@foxglove/rosmsg";
 
 const LICENSE = `// This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
